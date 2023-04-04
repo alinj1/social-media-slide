a96fab175288cd89776c387e52481dac8094096e
ali najafi <alinj3231@gmail.com>
2023-04-04 15:11:13 +0330

    add sochial project

---
+++ assest/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#contact_us_button").click(function () {
+
+        $("#contact_us_slide").slideToggle("slow");
+
+
+    })
+});
+
+$(document).ready(function () {
+    $("#contact_us_button").click(function () {
+
+        $("#contact_us_slide_open").slideToggle(0);
+        $("#contact_us_slide_close").slideToggle(0);
+
+
+
+    })
+
+
+
+});
